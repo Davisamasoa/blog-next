5a0d9ebcab031599ba0ae8f919786956d3685eee
Davi Samuel <davisamasoa@gmail.com>
2022-11-30 08:31:20 -0300

    responsive and bootstrap icon cdn

--- components/postPage.jsx
+++ components/postPage.jsx
@@ -7,10 +7,10 @@ import { Navbar } from "../components/navbar";
 export function PostPage(props) {
 	return (
 		<>
-			<article className=" md:w-[900px] sm:w-[700px]  w-full cursor-pointer mt-10 flex flex-col gap-5 justify-center items-start sm:p-5">
+			<article className=" md:w-[900px] sm:w-[700px]  w-full  cursor-pointer mt-10 flex flex-col gap-5 justify-center items-start sm:p-5">
 				<h2 className="sm:text-4xl text-center w-full text-[20px] text-white font-bold">{props.title} </h2>
 
-				<p dangerouslySetInnerHTML={{ __html: props.content }} className="text-white"></p>
+				<p dangerouslySetInnerHTML={{ __html: props.content }} className="text-white max-w-full"></p>
 				<span className="text-white ">{props.date}</span>
 			</article>
 		</>
--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,10 @@ export default function Document() {
 		<Html>
 			<Head>
 				<title>Blog</title>
+				<link
+					rel="stylesheet"
+					href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"
+				></link>
 			</Head>
 			<body>
 				<Main />
